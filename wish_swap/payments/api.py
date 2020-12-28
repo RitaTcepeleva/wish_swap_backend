17666f1fad4c5ae4e71f1a2690c324ed10c41c6f
yulian <khalitov.yulian@yandex.ru>
2020-12-28 18:44:55 +0300

    update logging

--- wish_swap/payments/api.py
+++ wish_swap/payments/api.py
@@ -1,7 +1,7 @@
 from wish_swap.payments.models import Payment
 from wish_swap.transfers.models import Transfer
 from wish_swap.transfers.api import eth_like_token_mint, binance_transfer
-from wish_swap.settings import BLOCKCHAINS
+from wish_swap.settings import BLOCKCHAINS, TOKEN_DECIMALS
 from wish_swap.rates.api import calculate_commission_in_wish
 from wish_swap.rates.models import WishCommission
 
@@ -41,17 +41,19 @@ def parse_payment(message):
         payment = Payment(address=from_address, tx_hash=tx_hash, currency=from_currency, amount=amount)
         payment.save()
         print(f'parsing payment: Payment {payment.tx_hash} from {payment.address} '
-              f'for {payment.amount} {payment.currency} successfully saved', flush=True)
+              f'for {payment.amount / TOKEN_DECIMALS} {payment.currency} successfully saved', flush=True)
 
         commission = calculate_commission_in_wish(to_blockchain, to_address, amount)
 
-        print(f'parsing payment: Transfer commission is {commission} WISH', flush=True)
+        print(f'parsing payment: Transfer commission is {commission / TOKEN_DECIMALS} WISH', flush=True)
         transfer = create_transfer(payment, to_address, to_currency, amount - commission)
 
         wish_commission_obj = WishCommission.objects.first() or WishCommission()
         wish_commission_obj.amount += commission
         wish_commission_obj.save()
-        print(f'parsing payment: Total commission amount is {wish_commission_obj.amount} WISH', flush=True)
+        print('parsing payment: Commission saved', flush=True)
+        print(f'parsing payment: Total commission amount is '
+              f'{wish_commission_obj.amount / TOKEN_DECIMALS} WISH', flush=True)
 
         if to_blockchain in ('Ethereum', 'Binance-Smart-Chain'):
             try:
@@ -61,14 +63,16 @@ def parse_payment(message):
                     amount=transfer.amount,
                 )
                 transfer.status = 'TRANSFERRED'
+                print(f'parsing payment: Successful transfer {transfer.tx_hash} to {transfer.address} '
+                      f'for {transfer.amount / TOKEN_DECIMALS} {transfer.currency}', flush=True)
                 transfer.save()
-                print(f'parsing payment: Transfer {transfer.tx_hash} to {transfer.address} '
-                      f'for {transfer.amount} {transfer.currency} successfully saved', flush=True)
+                print('parsing payment: Transfer saved', flush=True)
             except Exception as e:
                 transfer.tx_error = repr(e)
                 transfer.status = 'FAIL'
-                transfer.save()
                 print(f'parsing payment: Transfer failed with exception {transfer.tx_error}', flush=True)
+                transfer.save()
+                print('parsing payment: Transfer saved', flush=True)
         elif to_blockchain == 'Binance-Chain':
             binance_transfer(BLOCKCHAINS[to_blockchain], to_address, amount)
         else:
