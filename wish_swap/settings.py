b94966a924178d5f9c36cc5a1836e5d53639e8f8
yulian <khalitov.yulian@yandex.ru>
2021-01-04 23:29:02 +0300

    update static settings

--- wish_swap/settings.py
+++ wish_swap/settings.py
@@ -125,7 +125,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = os.getenv('DJANGO_STATIC_URL', '/static/')
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 GAS_LIMIT = 500000
 
