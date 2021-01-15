6d35a616a37f3d80aab3a54058e014981ed6a28f
yulian <khalitov.yulian@yandex.ru>
2021-01-15 13:53:41 +0300

    fix hardcode in scanner

--- scanner/networks/bin/network.py
+++ scanner/networks/bin/network.py
@@ -11,15 +11,13 @@ from scanner.blockchain_common.wrapper_network import WrapperNetwork
 from scanner.blockchain_common.wrapper_output import WrapperOutput
 from scanner.blockchain_common.wrapper_transaction import WrapperTransaction
 from scanner.blockchain_common.wrapper_transaction_receipt import WrapperTransactionReceipt
-from wish_swap.settings_local import NETWORKS, ERC20_TOKENS
-
+from wish_swap.settings_local import NETWORKS, ERC20_TOKENS, BLOCKCHAINS
 
 from binance_chain.http import HttpApiClient
 from binance_chain.http import PeerType
 from binance_chain.constants import KlineInterval
 from binance_chain.environment import BinanceEnvironment
 from binance_chain.node_rpc.http import HttpRpcClient
-
 
 client = HttpApiClient(request_params={"verify": False, "timeout": 20})
 client = HttpApiClient()
@@ -28,14 +26,15 @@ client = HttpApiClient(env=testnet_env)
 peers = client.get_node_peers()
 listen_addr = peers[0]['listen_addr']
 rpc_client = HttpRpcClient(listen_addr)
-processed=[]
-commited=[]
+processed = []
+commited = []
+
 
 class BinNetwork(WrapperNetwork):
     BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     sys.path.append(BASE_DIR)
     base_dir = 'scanner/settings'
-    
+
     def __init__(self, type):
         super().__init__(type)
         url = NETWORKS[type]['url']
@@ -43,38 +42,42 @@ class BinNetwork(WrapperNetwork):
 
     def get_last_block(self):
         pass
-    
+
     def get_block(self, s_time) -> WrapperBlock:
         print('BINANCE_MAINNET: scanning', flush=True)
-        client_transactions=client.get_transactions(address='tbnb1gz96khg7k20x84qup8jq669jx57ltzpz9aq2cw', tx_asset='WISHTEST-EA8', start_time=s_time, limit=1000)
-        tx_count=client_transactions['total']
+        client_transactions = client.get_transactions(address=BLOCKCHAINS['Binance-Chain']['token']['address'],
+                                                      tx_asset=BLOCKCHAINS['Binance-Chain']['token']['symbol'],
+                                                      start_time=s_time, limit=1000)
+        tx_count = client_transactions['total']
         print(tx_count, flush=True)
-        client_transactions=client_transactions['tx']
-        offset=0
-        i=0
-        while tx_count>len(client_transactions):
-            offset+=1000
-            client_transactions_append=client.get_transactions(address='tbnb1gz96khg7k20x84qup8jq669jx57ltzpz9aq2cw', tx_asset='WISHTEST-EA8', offset=offset, start_time=s_time, limit=1000)
-            tx_count=client_transactions_append['total']
-            client_transactions+=client_transactions_append['tx']
+        client_transactions = client_transactions['tx']
+        offset = 0
+        i = 0
+        while tx_count > len(client_transactions):
+            offset += 1000
+            client_transactions_append = client.get_transactions(
+                address=BLOCKCHAINS['Binance-Chain']['token']['address'],
+                tx_asset=BLOCKCHAINS['Binance-Chain']['token']['symbol'], offset=offset, start_time=s_time, limit=1000)
+            tx_count = client_transactions_append['total']
+            client_transactions += client_transactions_append['tx']
             time.sleep(1)
-            i+=1
-            if i>100:
+            i += 1
+            if i > 100:
                 print('got out of the loop')
                 break
         with open(os.path.join(self.base_dir, 'BINANCE_MAINNET'), 'r') as file:
             max_block = file.read()
-        if len(max_block)==0:
-            max_block=0
-        max_block=int(max_block)
-        new_transactions=[]
+        if len(max_block) == 0:
+            max_block = 0
+        max_block = int(max_block)
+        new_transactions = []
         for c_t in client_transactions:
-            if c_t['blockHeight']<=max_block:
+            if c_t['blockHeight'] <= max_block:
                 break
             new_transactions.append(c_t)
-        transactions=[]
+        transactions = []
         for t in new_transactions[::-1]:
-            if t['code']==0 and t['txType']=='TRANSFER':
+            if t['code'] == 0 and t['txType'] == 'TRANSFER':
                 output = WrapperOutput(
                     t['txHash'],
                     t['txAsset'],
@@ -83,18 +86,18 @@ class BinNetwork(WrapperNetwork):
                     t['memo']
                 )
                 transaction = WrapperTransaction(
-                        t['txHash'],
-                        t['fromAddr'],
-                        [output],
-                        False,
-                        ""
+                    t['txHash'],
+                    t['fromAddr'],
+                    [output],
+                    False,
+                    ""
                 )
 
                 transactions.append(transaction)
-                if int(t['blockHeight'])>max_block:
-                    max_block=int(t['blockHeight'])
-        block = WrapperBlock('','','', transactions)
-                
+                if int(t['blockHeight']) > max_block:
+                    max_block = int(t['blockHeight'])
+        block = WrapperBlock('', '', '', transactions)
+
         with open(os.path.join(self.base_dir, 'BINANCE_MAINNET'), 'w') as file:
             file.write(str(max_block))
         return block
