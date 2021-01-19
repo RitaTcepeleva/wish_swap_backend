4489b1c7f90c31f18c05078df144a4ad31c9bb1b
dsahdr <dshadr@mail.ru>
2021-01-19 14:42:56 +0300

    fixed mywish_models path to settings

--- scanner/mywish_models/models.py
+++ scanner/mywish_models/models.py
@@ -2,14 +2,14 @@ from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine
 
-from settings.settings_local import mywish_pg_engine
+from wish_swap.settings_local import mywish_pg_engine
 
 Base = automap_base()
 engine = create_engine(mywish_pg_engine)
 Base.prepare(engine, reflect=True)
 
 #CORRECT PATH TO MODELS
-Dex = Base.classes.dex_dex
-Token = Base.classes.dex_token
+Dex = Base.classes.tokens_dex
+Token = Base.classes.tokens_token
 
 session = Session(engine)
