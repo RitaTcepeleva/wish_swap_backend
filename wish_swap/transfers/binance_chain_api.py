c8b330f98c59a3b3cd9a415c7c18ca0efc039430
yulian-khalitov <khalitov.yulian@yandex.ru>
2021-01-19 19:03:36 +0300

    fix BinanceChainInterface

--- wish_swap/transfers/binance_chain_api.py
+++ wish_swap/transfers/binance_chain_api.py
@@ -8,7 +8,7 @@ class BinanceChainInterface:
 
     def multi_send(self, key, password, symbol, transfers):
         command_list = [
-            self.network['cli'], 'multi-send',
+            self.network['cli'], 'token', 'multi-send',
             '--from', key,
             '--chain-id', self.network['chain-id'],
             '--node', self.network['node'],
