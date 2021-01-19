2961cb1d075062b3e8e3e6ce269da03caea524a4
yulian-khalitov <khalitov.yulian@yandex.ru>
2021-01-19 14:35:32 +0300

    add on_delete arg to Payment's token field

--- wish_swap/payments/models.py
+++ wish_swap/payments/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Payment(models.Model):
-    token = models.ForeignKey('tokens.Token')
+    token = models.ForeignKey('tokens.Token', on_delete=models.CASCADE)
     address = models.CharField(max_length=100)
     tx_hash = models.CharField(max_length=100)
     amount = models.DecimalField(max_digits=100, decimal_places=0)
