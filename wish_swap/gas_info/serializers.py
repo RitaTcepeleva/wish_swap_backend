8976945c9292d6f8f52548ac2fd93dff7cc85769
yulian-khalitov <khalitov.yulian@yandex.ru>
2021-01-20 16:59:25 +0300

    fix GasInfoSerializer

--- wish_swap/gas_info/serializers.py
+++ wish_swap/gas_info/serializers.py
@@ -5,3 +5,11 @@ from wish_swap.gas_info.models import GasInfo
 class GasInfoSerializer(serializers.ModelSerializer):
     class Meta:
         model = GasInfo
+        extra_kwargs = {
+            'price': {'read_only': True},
+        }
+        fields = (
+            'network',
+            'price',
+            'price_limit',
+        )
