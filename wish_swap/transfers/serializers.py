0b2b6746becbdee1408d4c67f15e31f45af3276f
yulian-khalitov <khalitov.yulian@yandex.ru>
2021-01-21 03:14:21 +0300

    add payment_amount field to transfer serializer

--- wish_swap/transfers/serializers.py
+++ wish_swap/transfers/serializers.py
@@ -4,6 +4,7 @@ from wish_swap.transfers.models import Transfer
 
 class TransferSerializer(serializers.ModelSerializer):
     payment_hash = serializers.SerializerMethodField('get_payment_hash')
+    payment_amount = serializers.SerializerMethodField('get_payment_amount')
     token_address = serializers.SerializerMethodField('get_token_address')
     network = serializers.SerializerMethodField('get_network')
 
@@ -21,6 +22,9 @@ class TransferSerializer(serializers.ModelSerializer):
 
     def get_payment_hash(self, transfer):
         return transfer.payment.tx_hash
+
+    def get_payment_amount(self, transfer):
+        return transfer.payment.amount
 
     def get_token_address(self, transfer):
         return transfer.token.token_address
