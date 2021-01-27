bb79b845e90e654b4e0cd5702dd0fb715e8fb375
yulian-khalitov <khalitov.yulian@yandex.ru>
2021-01-27 22:55:40 +0300

    add timeout between transfers in celery pushing transfers task

--- celery_tasks.py
+++ celery_tasks.py
@@ -1,11 +1,17 @@
+import time
 from wish_swap.transfers.models import Transfer
 from wish_swap.networks.models import GasInfo
 from celery import shared_task
+from wish_swap.settings_local import PUSHING_TRANSFERS_TIMEOUT_SECS
 
 
 @shared_task
 def push_transfers():
     transfers = Transfer.objects.filter(status='HIGH GAS PRICE')
+    if not transfers.count():
+        print(f'PUSHING TRANSFERS: no transfers to push', flush=True)
+        return
+    print(f'PUSHING TRANSFERS: start pushing...', flush=True)
     for transfer in transfers:
         network = transfer.token.network
         if network in ('Ethereum', 'Binance-Smart-Chain'):
@@ -22,11 +28,12 @@ def push_transfers():
 
         if transfer.status == 'FAIL':
             print(f'PUSHING TRANSFERS: transfer failed with error {transfer.tx_error}', flush=True)
-            print('PUSHING TRANSFERS: aborting pushing', flush=True)
         else:
             decimals = (10 ** transfer.token.decimals)
             symbol = transfer.token.symbol
             print(f'PUSHING TRANSFERS: successful transfer {transfer.tx_hash} '
                   f'{transfer.amount / decimals} {symbol} to {transfer.address}, '
                   f'(fee) {transfer.fee_amount / decimals} {symbol} to {transfer.fee_address}', flush=True)
+
+        time.sleep(PUSHING_TRANSFERS_TIMEOUT_SECS)
     print(f'PUSHING TRANSFERS: pushing completed', flush=True)
