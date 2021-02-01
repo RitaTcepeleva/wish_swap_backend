a1519d192c8d95c913279b198267a8796e251bd1
dsahdr <dshadr@mail.ru>
2021-02-01 19:47:17 +0300

    disabling confirms BC

--- scanner/networks/bin/scanner.py
+++ scanner/networks/bin/scanner.py
@@ -32,11 +32,11 @@ class BinScanner(ScannerPolling):
                 self.process_block(block)
                 time.sleep(2)
             time.sleep(10)
-            transfers = session.query(Transfer).filter(getattr(Transfer, 'status').in_(status)).filter(getattr(Transfer, 'network').in_(network_types)).all()
+            '''transfers = session.query(Transfer).filter(getattr(Transfer, 'status').in_(status)).filter(getattr(Transfer, 'network').in_(network_types)).all()
             print(f'len:{len(transfers)}')
             for transfer in transfers:
                 confirm=self.network.confirm_transfer(transfer)
-                time.sleep(2)
+                time.sleep(2)'''
         print('got out of the main loop')
 
     
