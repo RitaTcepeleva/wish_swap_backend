d668801a754195d5756180e59cee9a0bcd5c9a92
dsahdr <dshadr@mail.ru>
2021-02-02 12:53:00 +0300

    implemented scanner bot messages

--- scanner/networks/bin/scanner.py
+++ scanner/networks/bin/scanner.py
@@ -6,7 +6,7 @@ from scanner.eventscanner.queue.subscribers import pub
 from scanner.scanner.events.block_event import BlockEvent
 from scanner.scanner.services.scanner_polling import ScannerPolling
 from scanner.mywish_models.models import Dex, Token, Transfer, session
-
+from scanner.eventscanner.queue.pika_handler import send_to_backend
 
 class BinScanner(ScannerPolling):
 
@@ -19,26 +19,49 @@ class BinScanner(ScannerPolling):
     def polling(self):
         network_types = ['Binance-Chain', ]
         status=['WAITING FOR CONFIRM']
-        tokens = session.query(Token).filter(getattr(Token, 'network').in_(network_types)).all()
-        for token in tokens:
-            swap_address = token.swap_address
-            block=self.network.get_block(token, swap_address, int(time.time()*1000-604800000))
-            self.process_block(block)
-            time.sleep(2)
-        while True:
+        try:
+            tokens = session.query(Token).filter(getattr(Token, 'network').in_(network_types)).all()
             for token in tokens:
                 swap_address = token.swap_address
-                block = self.network.get_block(token, swap_address, int(time.time() * 1000 - 604800000))
+                block=self.network.get_block(token, swap_address, int(time.time()*1000-604800000))
                 self.process_block(block)
                 time.sleep(2)
+            while True:
+                for token in tokens:
+                    swap_address = token.swap_address
+                    block = self.network.get_block(token, swap_address, int(time.time() * 1000 - 604800000))
+                    self.process_block(block)
+                    time.sleep(2)
+                time.sleep(10)
+                #disabled confirms
+                '''transfers = session.query(Transfer).filter(getattr(Transfer, 'status').in_(status)).filter(getattr(Transfer, 'network').in_(network_types)).all()
+                print(f'len:{len(transfers)}')
+                for transfer in transfers:
+                    confirm=self.network.confirm_transfer(transfer)
+                    time.sleep(2)'''
+                try:
+                    with open(os.path.join(self.base_dir, 'Binance-Chain', 'status'), '+') as file:
+                        status = file.read()
+                        if status == 'dead':
+                            send_to_backend('scanner-up', 'Binance-Chain-bot', 'scanner ressurected')
+                        file.write('alive')
+                except FileNotFoundError:
+                    pass
+            print('got out of the main loop')
+        except:
+            send_to_backend('scanner-crash', 'Binance-Chain-bot', 'scanner is dead')
+            try:
+                with open(os.path.join(self.base_dir, self.type, 'status'), 'w') as file:
+                    file.write('dead')
+                return block
+            except FileNotFoundError:
+                print('except creation')
+                filename = os.path.join(self.base_dir, self.type)
+                os.makedirs(filename, exist_ok=True)
+                with open(os.path.join(self.base_dir, self.type, 'status'), 'w') as file:
+                    file.write('dead')
+                return block
             time.sleep(10)
-            #disabled confirms
-            '''transfers = session.query(Transfer).filter(getattr(Transfer, 'status').in_(status)).filter(getattr(Transfer, 'network').in_(network_types)).all()
-            print(f'len:{len(transfers)}')
-            for transfer in transfers:
-                confirm=self.network.confirm_transfer(transfer)
-                time.sleep(2)'''
-        print('got out of the main loop')
 
     
     def process_block(self, block: WrapperBlock):
